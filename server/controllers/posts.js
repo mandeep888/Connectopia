d3ea0795f8d8224cfac12ab433f630f9c40c4622
Mandeep <Mandeepsinghjass@gmail.com>
2024-03-07 23:18:32 +0530

    Created a Folder structure for Routes , mongooose models and controllers to handle the routes logic.

---
+++ server/controllers/posts.js
@@ -0,0 +1,8 @@
+import postMessage from "../models/postMessage";
+
+export const getPosts = (req, res) => {
+    res.send('This works!');
+}
+export const createPost = (req, res) => {
+    res.send('Post Creation!');
+}
--- server/index.js
+++ server/index.js
@@ -3,9 +3,11 @@ import express from "express"
 import bodyParser from "body-parser"
 import cors from "cors"
 import mongoose from "mongoose"
+import postRoutes from "./Routes/posts.js"
 
 const app = express();
 
+app.use('/posts',postRoutes)
 app.use(bodyParser.json({limit:"30mb" , extended:"true"}));
 app.use(bodyParser.urlencoded({limit:"30mb" , extended:"true"}));
 app.use(cors)
