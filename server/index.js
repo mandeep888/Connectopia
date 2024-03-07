4d4ed8677737c8808438fd535a250bef0c4cbd4e
Mandeep <Mandeepsinghjass@gmail.com>
2024-03-07 21:24:43 +0530

    Successfully connected to the MongoDB Atlas using Mongoose

--- server/index.js
+++ server/index.js
@@ -9,3 +9,13 @@ const app = express();
 app.use(bodyParser.json({limit:"30mb" , extended:"true"}));
 app.use(bodyParser.urlencoded({limit:"30mb" , extended:"true"}));
 app.use(cors)
+
+//DATABASE CONNECTIVITY (MONGODB ATLAS)
+const CONNECTION_URL = "mongodb+srv://mandeepsinghjass:e9fY7LfzToYru6GA@memoriescluster.sskqjjt.mongodb.net/?retryWrites=true&w=majority&appName=MemoriesCluster"
+const PORT = process.env.PORT  || 5000; //process.env is a built in object that allows access to environment variables 
+
+mongoose.connect(CONNECTION_URL , {useNewUrlParser:true , useUnifiedTopology : true})
+.then(()=> app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`)))
+.catch((error)=> console.log(error.message))
+
+// mongoose.set('useFindAndModify', false); //to avoid error in node v12+ but now this is deprecated
