2ea316868f5b15ea1d8b42393431ef8e7172d116
Mandeep <Mandeepsinghjass@gmail.com>
2024-03-08 20:39:35 +0530

    Added Post and form components for the app and added a basic design in app.js using materialUI

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,32 @@
 import react from 'react'
-
+import {Container , AppBar , Grow ,Grid ,Typography} from '@material-ui/core'
+import memories from "./images/memories.png"
+import Form from "./components/Form/Form"
+import Posts from "./components/Posts/Posts"
+import useStyles from './styles'
 const App = ()=>{
+    const classes = useStyles();
     return(
         <div>
-            <h1>APP</h1>
+            <Container maxWidth='lg'>
+                <AppBar className={classes.appBar} position='static' color='inherit'>
+                    <Typography className={classes.heading} variant='h2' align='center'> Memories
+                        <img src={memories} className={classes.image} alt="memories" height='60'/>
+                    </Typography>
+                </AppBar>
+                <Grow in>
+                    <Container>
+                        <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+                            <Grid item xs={12} sm={7}>
+                                <Posts />
+                            </Grid>
+                            <Grid item xs={12} sm={4}>
+                                <Form />
+                            </Grid>
+                        </Grid>
+                    </Container>
+                </Grow>
+            </Container>
         </div>
     )
 }
