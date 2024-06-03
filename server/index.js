958c0f4b46c34a23010a42c545511a5fc9e5bbdb
Mandeep <Mandeepsinghjass@gmail.com>
2024-06-03 23:10:19 +0530

    Added Middleware and Api for user authentication

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose"
 import bodyParser from "body-parser"
 import cors from "cors"
 import postRoutes from "./Routes/posts.js"
+import userRoutes from "./Routes/users.js"
 import dotenv from "dotenv"
 
 const app = express();
@@ -13,6 +14,7 @@ app.use(bodyParser.urlencoded({limit:"30mb" , extended:"true"}));
 app.use(cors());
 app.use(express.json());
 app.use('/posts',postRoutes)
+app.use('/user',userRoutes)
 
 
 const PORT = process.env.PORT|| 5000; //process.env is a built in object that allows access to environment variables 
